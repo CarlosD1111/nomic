0f93101cfb1de1a97ae14cee5fecac1ae71ac9b3
CarlosD1111 <132258585+CarlosD1111@users.noreply.github.com>
2023-05-15 13:34:21 +0400

    Update gpt4all.py

--- nomic/gpt4all/gpt4all.py
+++ nomic/gpt4all/gpt4all.py
@@ -15,7 +15,7 @@ except ImportError:
 class GPT4AllGPU():
     def __init__(self, llama_path=None):
         from peft import PeftModelForCausalLM
-        from transformers import AutoModelForCausalLM, AutoTokenizer
+        from transformers import LlamaForCausalLM, LlamaTokenizer
 
         if llama_path is None:
             raise ValueError('Please pass a path to your alpaca model.')
@@ -23,10 +23,10 @@ class GPT4AllGPU():
         self.model_path = llama_path
         self.tokenizer_path = llama_path
         self.lora_path = 'nomic-ai/vicuna-lora-multi-turn_epoch_2'
-        self.model = AutoModelForCausalLM.from_pretrained(self.model_path,
+        self.model = LlamaForCausalLM.from_pretrained(self.model_path,
                                                           device_map="auto",
                                                           torch_dtype=torch.float16)
-        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_path)
+        self.tokenizer = LlamaTokenizer.from_pretrained(self.tokenizer_path)
         added_tokens = self.tokenizer.add_special_tokens({"bos_token": "<s>", "eos_token": "</s>", "pad_token": "<pad>"})
 
         if added_tokens > 0:
